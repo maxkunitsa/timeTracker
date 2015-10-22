32fb0d5b4e4e46ab23ae0838a6f3f665412ec26a
maxkunitsa <maksym.kunytsia@fabware.com>
2015-10-22 21:30:17 +0300

    + added translate library

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -10,19 +10,37 @@
       'ngRoute',
       'ngSanitize',
       'ngTouch',
-      'ui.router'
+      'ui.router',
+      'pascalprecht.translate'
     ])
-    .config(function ($stateProvider, $urlRouterProvider) {
+    .config(AppConfig)
+    .controller('AppCtrl', AppCtrl);
+
+    function AppConfig ($stateProvider, $urlRouterProvider) {
       $stateProvider
         .state('login', {
           url: '/',
           templateUrl: 'views/login.html',
           controller: 'LoginCtrl'
+        })
+        .state('timesheet', {
+          url: '/timesheet',
+          templateUrl: 'views/timesheet/timesheet.html',
+          controller: 'TimeSheetCtrl'
         });
 
       // if none of the above states are matched, use this as the fallback
-      $urlRouterProvider.otherwise('/');
-      
-    });
+      $urlRouterProvider.otherwise('/timesheet')
+    }
+
+    function AppCtrl ($rootScope) {
+      $rootScope.GlobalParams = {
+        isAuthenticated: true,
+        navExpanded: true
+      }
+    }
+
+
+
 
 })();
