a5f089b1c9ce31ff77a3dc8522d0485034bedcba
Aleksandr Savvopulo <contact@hexwit.com>
2015-11-07 19:32:49 +0200

    Implemented getting, checking for existence and deleting project by id.

--- server/src/main/java/services/ProjectService.java
+++ server/src/main/java/services/ProjectService.java
@@ -5,11 +5,11 @@ import com.google.inject.Singleton;
 import exceptions.Exceptions;
 import models.Project;
 import ninja.jongo.annotations.InjectMongoCollection;
+import org.bson.types.ObjectId;
 import org.jongo.MongoCollection;
 import org.jongo.MongoCursor;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -38,6 +38,25 @@ public class ProjectService {
     }
 
     /**
+     * Returns project with id specified
+     *
+     * @return project entity
+     */
+    public Project getById(String projectId) {
+        return projects.findOne(new ObjectId(projectId)).as(Project.class);
+    }
+
+    /**
+     * Checks is project with specified id exists.
+     *
+     * @param projectId id of the project
+     * @return true if project exists, false - otherwise
+     */
+    public boolean isExists(String projectId) {
+        return projects.count("{_id: #}", new ObjectId(projectId)) > 0;
+    }
+
+    /**
      * Creating a new project.
      *
      * @param project project to be created
@@ -47,5 +66,23 @@ public class ProjectService {
         projects.insert(project);
 
         return project;
+    }
+
+    /**
+     * Deleting specific project from the system.
+     *
+     * @param project project to be deleted
+     */
+    public void delete(Project project) {
+        delete(project.getId());
+    }
+
+    /**
+     * Deleting specific project from the system.
+     *
+     * @param projectId id of project to be deleted
+     */
+    public void delete(String projectId) {
+        projects.remove(new ObjectId(projectId));
     }
 }
