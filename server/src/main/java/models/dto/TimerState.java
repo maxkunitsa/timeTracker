c1ac4ce9ae9f46b89b2cf37a3a592aeb03626c2e
Aleksandr Savvopulo <contact@hexwit.com>
2015-11-26 23:18:45 +0200

    Added a DTO for timer state representation

--- server/src/main/java/models/dto/TimerState.java
+++ server/src/main/java/models/dto/TimerState.java
@@ -1,21 +1,13 @@
 package models.dto;
 
-import com.fasterxml.jackson.annotation.JsonView;
-import models.serialization.JsonViews;
-import ninja.validation.Required;
-
 /**
- * Represents a current state of the timer.
+ * Represents a timer state DTO.
  * <p/>
  * Author: Aleksandr Savvopulo
  * Date: 22.11.2015
  */
 public class TimerState {
     private boolean isRunning = false;
-
-    public TimerState() {
-
-    }
 
     public boolean isRunning() {
         return isRunning;
